1977d7d8f082f3135b8969d072c7a97a9fd32cb1
Anirudh <anirudh.das2903@outlook.in>
2024-05-09 23:55:33 +0530

    started react-native app with expo & nativewind

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet , Text, View } from "react-native";
+import {StatusBar} from 'expo-status-bar';
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Dreamscape</Text>
+      <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
