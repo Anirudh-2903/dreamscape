93fbffe298269e4ce2fa6358831bff94f39d5979
Anirudh <anirudh.das2903@outlook.in>
2024-05-11 00:08:37 +0530

    added auth layout & sign in and up routes

--- app/index.jsx
+++ app/index.jsx
@@ -6,10 +6,9 @@ import { useFonts } from 'expo-font';
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-3xl">Dreamscape</Text>
+      <Text className="text-3xl font-pblack">Dreamscape</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
     </View>
   );
 }
-
