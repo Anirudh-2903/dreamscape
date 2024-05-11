e447f87a96a832a922ed779e4bdeab94b9ef593d
Anirudh <anirudh.das2903@outlook.in>
2024-05-11 08:33:54 +0530

    added onboarding page and components folder

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,27 @@
-import {  Text, View } from "react-native";
+import {  Image, ScrollView, Text, View } from "react-native";
 import {StatusBar} from 'expo-status-bar';
 import { Link } from "expo-router";
 import { useFonts } from 'expo-font';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-3xl font-pblack">Dreamscape</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{color: 'blue'}}>Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full" style={{flex: 1}}>
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <View className="h-full w-full items-center justify-center px-4">
+          <Image source={images.logo} className="w-[300px] h-[100px]" resizeMode="contain" />
+          <Image source={images.cards} className="max-w-[380px] w-full h-[300px]" resizeMode="contain" />
+          <View className="relative mt-5">
+            <Text className="text-center text-3xl font-bold text-white">Discover endless realms with {' '}
+              <Text className="text-secondary-200">Dreamscape</Text>
+            </Text>
+            <Image source={images.path} className="absolute -bottom-6 right-16 w-[136px] h-[35px]" resizeMode="contain" />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 text-center mt-7">Where imagination meets innovation: embark on a voyage of endless exploration with Dreamscape</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
